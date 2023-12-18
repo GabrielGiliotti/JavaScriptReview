9de0350db4b408e06bd4c4b47904e8b966c40349
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2023-12-18 20:48:38 -0300

    inicio lib/projeto para verificacao links em arquivos markdown

---
+++ LinkLib/index.js
@@ -0,0 +1,9 @@
+// Para iniciar projeto Node
+// npm init              ou
+// yarn init
+
+// Para instalar pacotes
+// npm install <nome do pacote>    ou 
+// yarn add <nome do pacote>
+
+console.log("Hello World");
