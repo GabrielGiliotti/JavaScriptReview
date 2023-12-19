a878613343a274a68c6ff473a9ac2a26d6fdea2b
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2023-12-18 21:01:10 -0300

    importacao modulo chalk para colorir textos no terminal do node

--- LinkLib/index.js
+++ LinkLib/index.js
@@ -6,4 +6,8 @@
 // npm install <nome do pacote>    ou 
 // yarn add <nome do pacote>
 
+import chalk from 'chalk';  // equivalente a const chalk = require('chalk');
+
 console.log("Hello World");
+
+console.log(chalk.blue('Hello world!'));
