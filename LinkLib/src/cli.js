772392ad52dccff75a3bafc3d5c39dcb41492cbf
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2023-12-20 11:04:35 -0300

    aplicando retorno de links para todos os arquivos em uma pasta

--- LinkLib/src/cli.js
+++ LinkLib/src/cli.js
@@ -1,11 +1,27 @@
 import chalk from 'chalk';
+import fs from 'fs';
 import getFile from './index.js';
 
-const path = process.argv;
+const args = process.argv;
 
-async function processText(path) {
-    const result = await getFile(path[2]);
+function printResult(result) {
     console.log(chalk.yellow('Links list:'), result);
 }
 
-processText(path);
+async function processText(args) {
+    const path = args[2];
+
+    if(fs.lstatSync(path).isFile()) {
+        const result = await getFile(args[2]);
+        printResult(result);
+    }
+    else if (fs.lstatSync(path).isDirectory()) {
+        const files = await fs.promises.readdir(path);
+        files.forEach(async (fileName) => {
+            const list = await getFile(`${path}/${fileName}`);
+            printResult(list);
+        })
+    }
+}
+
+processText(args);
