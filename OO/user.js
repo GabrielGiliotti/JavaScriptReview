e217d23bf269a2a6b7ea95eccc93032c95e95984
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2023-12-25 23:28:48 -0300

    trabalhando com classe User

---
+++ OO/user.js
@@ -0,0 +1,19 @@
+class User {
+    constructor(name, email, birthDate, role, active = true) {
+        this.name = name
+        this.email = email 
+        this.birthDate = birthDate 
+        this.role = role || 'student' 
+        this.active = active
+    }
+
+    displayInfo() {
+        return `${this.name}, ${this.email}`
+    }
+}
+
+const newUser = new User('Gabriel', 'gvg@email.com', '2023-12-25');
+console.log(newUser);
+console.log(newUser.displayInfo());
+
+// console.log(User.prototype.isPrototypeOf(newUser));
