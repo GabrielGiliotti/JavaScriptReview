d3d454c3d104f90b0776fb2cd39c9c6c6ce10054
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2024-01-04 14:51:29 -0300

    utilizacao de getters e setters

--- OO/Classes/index.js
+++ OO/Classes/index.js
@@ -2,6 +2,14 @@ import User from './user.js'
 import Admin from './admin.js'
 import Docente from './docente.js'
 
-const user = new User('Gabriel', 'g@g.com');
+const user = new User('Gabriel', 'g@g.com', '2024-01-04');
 console.log(user.displayInfo());
+
+const admin = new Admin('Clara', 'c@c.com', '2024-01-04');
+//console.log(admin.displayInfo());
+
+console.log(admin.name);
+admin.name = 'Flavia';
+console.log(admin.name);
+
 
--- OO/Classes/user.js
+++ OO/Classes/user.js
@@ -1,16 +1,64 @@
 export default class User {
     #name
     #email
+    #birthDate
+    #role
+    #active
     constructor(name, email, birthDate, role, active = true) {
         this.#name = name
         this.#email = email 
-        this.birthDate = birthDate 
-        this.role = role || 'student' 
-        this.active = active
+        this.#birthDate = birthDate 
+        this.#role = role || 'student' 
+        this.#active = active
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    get birthDate() {
+        return this.#birthDate;
+    }
+
+    get role() {
+        return this.#role;
+    }
+    
+    get active() {
+        return this.#active;
+    }
+
+    set name(newName) {
+        if(newName === '') throw new Error('Invalid Format');
+        this.#name = newName;
+    }
+
+    set email(newEmail) {
+        if(newEmail === '') throw new Error('Invalid Format');
+        this.#email = newEmail;
+    }
+
+    set birthDate(newBirthDate) {
+        if(newBirthDate === '') throw new Error('Invalid Format');
+        this.#birthDate = newBirthDate;
+    }
+
+    set role(newRole) {
+        if(newRole === '') throw new Error('Invalid Format');
+        this.#role = newRole;
+    }
+    
+    set active(newActive) {
+        if(newActive === null || newActive === undefined) throw new Error('Invalid Format');
+        this.#active = newActive;
     }
 
     displayInfo() {
-        return `${this.#name}, ${this.#email}`
+        return `${this.name}, ${this.email}, ${this.birthDate}, ${this.role}, ${this.active}`
     }
 }
 
